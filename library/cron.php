2f2e3c48b93357c34fe566cfe75f9905b3022c1f
FenghenHome <FenghenHome@users.noreply.github.com>
2016-03-18 18:24:26 +0800

    Update cron.php

--- library/cron.php
+++ library/cron.php
@@ -1,13 +1,25 @@
 <?php
-自动重置过期用户流量，需要加入计划任务
-$conn = @mysql_connect("localhost","xxxx","xxxx");//数据库地址，用户名，密码
-mysql_select_db("xx", $conn);//数据库
-mysql_query("set user set 'utf8'");//xxx为数据库表名，数据库编码 
-$expired_time = "select expired_time from user";//需要查询表的字段
-$result = mysql_query($expired_time);//查询字段
+//自动重置过期用户流量，需要加入计划任务，每日执行一次！
+
+
+
+//连接数据库，地址，用户名，密码
+$conn = @mysql_connect("localhost","xxxx","xxxx");
+//选择数据库，xxxx为数据库名
+mysql_select_db("xxxx", $conn);
+//设置数据库编码
+mysql_query("set user set 'utf8'");
+//选择过期时间字段
+$expired_time = "select expired_time from user";
+//查询过期时间字段
+$result = mysql_query($expired_time);
+//循环获取过期时间字段
 while($expiredtime=mysql_fetch_array($result)){
+    //获取过期时间字段
     $time=$expiredtime["expired_time"];
+    //转换为日期
     $expired_date=date('Ymd',$time);
+    //获取当前日期
     $now_date=date('Ymd');
     //当前日期和过期日期一样时候，流量重置为0
     if ($now_date==$expired_date){
