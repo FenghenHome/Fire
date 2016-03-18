f6a321c9145f483aadafd78c5384af82fc9ba819
FenghenHome <FenghenHome@users.noreply.github.com>
2016-03-18 18:38:52 +0800

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,5 +1,5 @@
 <footer class="small-text center">
-	<p>Fire 开源在 <a href="https://github.com/ss098/Fire">Github</a></p>
+	<p>Fire 开源在 <a href="https://github.com/FenghenHome/Fire">Github</a></p>
 </footer>
 </body>
 </html>
