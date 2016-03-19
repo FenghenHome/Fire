db7cf3bbb781ba35c01e877f8253eee6934950b9
FenghenHome <FenghenHome@users.noreply.github.com>
2016-03-19 17:09:35 +0800

    Update pay.php

--- views/pay.php
+++ views/pay.php
@@ -28,7 +28,7 @@
 			</form>
 			<ol>
 				<li>向 <a href="https://auth.alipay.com/login/index.htm?goto=https%3A%2F%2Fshenghuo.alipay.com%2Fsend%2Fpayment%2Ffill.htm" target="_blank"><code>591425752@qq.com</code></a> 转账一个整数，小数部分将被忽略，使用手机支付宝可以免除手续费</li>
-				<li>访问 <a href="https://consumeprod.alipay.com/record/advanced.htm" target="_blank"><code>https://consumeprod.alipay.com/record/advanced.htm</code></a> 查看支付宝流水号粘贴到输入框中</li>
+				<li>访问 <a href="https://consumeprod.alipay.com/record/advanced.htm" target="_blank"><code>https://consumeprod.alipay.com/record/advanced.htm</code></a> 查看支付宝流水号粘贴到输入框中，请注意该页面是支付宝交易记录高级版，直接访问会跳转到普通版</li>
 			</ol>
 			<p>客服电话是 <span class="badge">100 0000 0000</span> ，服务时间是 08：00 至 20：00</p>
 		</div>
