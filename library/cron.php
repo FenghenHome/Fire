a7bd5859614ad8895a0a52c9264be2579c113ca3
FenghenHome <FenghenHome@users.noreply.github.com>
2016-03-19 14:19:39 +0800

    Update cron.php

--- library/cron.php
+++ library/cron.php
@@ -17,12 +17,13 @@ function generate_string($length=16) {
 	return $password;
 }
 
-//自动要改密码的用户名
-$username="xxxx";
+
 //连接数据库，地址，用户名，密码
 $conn = @mysql_connect("localhost","xxxx","xxxx");
 //选择数据库，xxxx为数据库名
 mysql_select_db("xxxx", $conn);
+//自动要改密码的用户名
+$username="xxxx";
 //设置数据库编码
 mysql_query("set user set 'utf8'");
 //随机密码
@@ -38,23 +39,23 @@ $expired_time = "select expired_time from user";
 //查询过期时间字段
 $result = mysql_query($expired_time);
 //循环获取过期时间字段
-while($expiredtime=mysql_fetch_array($result)){
-    //获取过期时间字段
-    $time=$expiredtime["expired_time"];
-    //转换为日期
-    $expired_date=date('Ymd',$time);
-    //获取当前日期
-    $now_date=date('Ymd');
-    //当前日期和过期日期一样时候，流量重置为0
-    if ($now_date==$expired_date){
-        //流量为0
-        $transfer_enable = 0;
-        //流量重置
-        $sql = "UPDATE user SET transfer_enable = '".$transfer_enable."' WHERE expired_time = '".$time."'";
-        //流量重置
-        mysql_query($sql);
+	while($expiredtime=mysql_fetch_array($result)){
+	    //获取过期时间字段
+	    $time=$expiredtime["expired_time"];
+	    //转换为日期
+	    $expired_date=date('Ymd',$time);
+	    //获取当前日期
+ 	   $now_date=date('Ymd');
+ 	   //当前日期和过期日期一样时候，流量重置为0
+ 	   if ($now_date==$expired_date){
+ 	       //流量为0
+ 	       $transfer_enable = 0;
+ 	       //流量重置
+    	    $sql = "UPDATE user SET transfer_enable = '".$transfer_enable."' WHERE expired_time = '".$time."'";
+   	     //流量重置
+  	      mysql_query($sql);
         echo "done";
-    } else {
+  	  } else {
         echo "ERROR";
-    }
-}
+ 	   }
+	}
