04cac3f80b1df388d6d04b17ebc563e4907a0a6c
FenghenHome <FenghenHome@users.noreply.github.com>
2016-03-26 14:47:41 +0800

    Update pay.php

--- views/pay.php
+++ views/pay.php
@@ -29,7 +29,7 @@
 				<li>向 <a href="https://auth.alipay.com/login/index.htm?goto=https%3A%2F%2Fshenghuo.alipay.com%2Fsend%2Fpayment%2Ffill.htm" target="_blank"><code>s.sunfeihu@gmail.com</code></a> 转账一个整数，小数部分将被忽略，使用手机支付宝可以免除手续费</li>
 				<li>访问 <a href="https://consumeprod.alipay.com/record/advanced.htm" target="_blank"><code>https://consumeprod.alipay.com/record/advanced.htm</code></a> 查看支付宝流水号粘贴到输入框中，请注意该页面是支付宝交易记录高级版，直接访问会跳转到普通版</li>
 			</ol>
-			<p>客服电话是 <span class="badge">100 0000 0000</span> ，服务时间是 08：00 至 20：00</p>
+			<p>客服邮箱是 <span class="badge">s.sunfeihu@gmail.com</span> ，服务时间是 08：00 至 20：00</p>
 		</div>
 		<div class="card-footer small-text">
 			<p>&copy; <?php echo date('Y'); ?> <a href="http://cenegd.com/">cenegd</a></p>
